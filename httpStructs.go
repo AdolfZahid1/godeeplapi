845d5e4308eae94cbeedcbaa0fc62d70451748d4
AdolfZahid1 <k.riobod@gmail.com>
2025-04-20 21:45:33 +0300

    Add TranslateFile function

--- httpStructs.go
+++ httpStructs.go
@@ -37,12 +37,7 @@ type TranslationRequest struct {
 	// Sets whether the translated text should lean towards formal or informal language.
 	// This feature is only available for certain target languages.
 	// Setting this parameter with a target language that does not support formality will fail,
-	// unless one of the prefer_... options are used. Possible options are:
-	//   default (default)
-	//   more - for a more formal language
-	//   less - for a more informal language
-	//   prefer_more - for a more formal language if available, otherwise fallback to default formality
-	//   prefer_less - for a more informal language if available, otherwise fallback to default formality
+	// unless one of the prefer_... options are used. Use Formality struct for possible options
 	Formality string `json:"formality,omitempty"`
 
 	// Specifies which DeepL model should be used for translation.
@@ -73,6 +68,46 @@ type TranslationRequest struct {
 
 	// Comma-separated list of XML tags that indicate text not to be translated.
 	IgnoreTags []string `json:"ignore_tags,omitempty"`
+}
+
+type FileTranslationRequest struct {
+	// Language of the text to be translated.
+	// If this parameter is omitted, the API will attempt to detect the language of the text and translate it.
+	SourceLang string `json:"source_lang,omitempty"`
+
+	// The language into which the text should be translated.
+	// Use TargetLanguageCode struct to define the lang
+	TargetLang string `json:"target_lang"`
+
+	//The document file to be translated. The file name should be included in this part's content disposition. As an alternative, the filename parameter can be used. The following file types and extensions are supported:
+	//
+	//    docx - Microsoft Word Document
+	//    pptx - Microsoft PowerPoint Document
+	//    xlsx - Microsoft Excel Document
+	//    pdf - Portable Document Format
+	//    htm / html - HTML Document
+	//    txt - Plain Text Document
+	//    xlf / xliff - XLIFF Document, version 2.1
+	//    srt - SRT Document
+	File io.Reader `json:"-"`
+
+	//The name of the uploaded file. Can be used as an alternative to including the file name in the file part's content disposition.
+	FileName string `json:"filename,omitempty"`
+
+	//File extension of desired format of translated file, for example: docx. If unspecified, by default the translated file will be in the same format as the input file.
+	OutputFormat string `json:"output_format,omitempty"`
+
+	// Sets whether the translated text should lean towards formal or informal language.
+	// This feature is only available for certain target languages.
+	// Setting this parameter with a target language that does not support formality will fail,
+	// unless one of the prefer_... options are used. Use Formality struct for possible options
+	Formality string `json:"formality,omitempty"`
+
+	// Specify the glossary to use for the translation.
+	// Important: This requires the source_lang parameter to be set.
+	// The language pair of the glossary has to match the language pair of the request.
+	// Example: def3a26b-3e84-45b3-84ae-0c0aaf3525f7
+	GlossaryId string `json:"glossary_id,omitempty"`
 }
 
 // TranslationResponse represents the response from the translation API.
