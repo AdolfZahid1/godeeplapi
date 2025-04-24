eb2d0a397adac1249745543cca477b1e4454e32c
AdolfZahid1 <k.riobod@gmail.com>
2025-04-25 00:19:34 +0300

    Add new model type for usage quotas. Add functions to retrieve usage quota and supported languages

---
+++ misc.go
@@ -0,0 +1,43 @@
+package godeeplapi
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/AdolfZahid1/godeeplapi/models"
+)
+
+// GetUsageAndLimits returns characters translated in the current billing period and current maximum number of characters that can be translated per billing period.
+func (c *Client) GetUsageAndLimits(ctx context.Context) (*models.UsageAndLimitResponse, error) {
+	err := c.checkAuth()
+	if err != nil {
+		return nil, err
+	}
+	respBody, err := c.doRequestWithQuery(ctx, "GET", "/usage", nil, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	var response models.UsageAndLimitResponse
+	err = json.Unmarshal(respBody, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+// GetLanguages returns struct with all supported languages
+func (c *Client) GetLanguages(ctx context.Context) (*models.LanguagesResponse, error) {
+	err := c.checkAuth()
+	if err != nil {
+		return nil, err
+	}
+	respBody, err := c.doRequestWithQuery(ctx, "GET", "/languages", nil, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	var response models.LanguagesResponse
+	err = json.Unmarshal(respBody, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
